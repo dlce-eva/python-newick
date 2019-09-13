6db59453a4f9a4280e7b89ef39792f0790b7f948
xrotwang <xrotwang@googlemail.com>
2019-09-13 15:09:41 +0200

    bump version for development

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='newick',
-    version="1.0.0",
+    version="1.0.1.dev0",
     description='A python module to read and write the Newick format',
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
